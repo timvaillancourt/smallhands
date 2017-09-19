8ba1fee6a0d404140da2ce452b956da9ffc00fb8
Tim Vaillancourt <tim@timvaillancourt.com>
2017-09-19 02:39:50 +0200

    cleanup

--- smallhands/worker.py
+++ smallhands/worker.py
@@ -4,17 +4,20 @@ from multiprocessing import Process
 
 
 class Find(Process):
-    def __init__(self, config, queue, do_stop, sleep_secs=0.50):
+    def __init__(self, config, queue, do_stop, sleep_secs=0.50, min_queued=5):
         Process.__init__(self)
         self.config     = config
         self.queue      = queue
         self.do_stop    = do_stop
         self.sleep_secs = sleep_secs
+        self.min_queued = min_queued
 
     def run(self):
-        logging.info("Starting .find() worker")
+        logging.info("Starting point .find() worker")
         while not self.do_stop.is_set():
-            if not self.queue.empty() and self.queue.qsize() > 10:
+            queued = self.queue.qsize()
+            if queued > self.min_queued:
                 data = self.queue.get(False, 1)
                 if data:
-                    print("find", data)
+                    print("find: %s (queued=%i)" % (data, queued - self.min_queued))
+        logging.info("Stopped point .find() worker")
